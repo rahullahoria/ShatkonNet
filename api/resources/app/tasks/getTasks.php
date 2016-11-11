0deb6f21f7c625e07bb1e06d31b6c0b70bfcaeef
anil-ninja <kumar.anil8892@gmail.com>
2016-11-11 13:02:58 +0530

    test

--- api/resources/app/tasks/getTasks.php
+++ api/resources/app/tasks/getTasks.php
@@ -21,7 +21,7 @@ function getTasks($appId){
         $stmt->bindParam("app_id", $appId);
         $stmt->execute();
         $url = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        print_r($url);
+        print_r($url[0]['fetch_url']);
         /*$ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, '$url->("fetch_url")');
         curl_setopt($ch, CURLOPT_HEADER, 0);            
