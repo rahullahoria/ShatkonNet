d6238d5c07719c4d004e808114dfbdeddf7a3778
anil-ninja <kumar.anil8892@gmail.com>
2016-11-14 17:26:10 +0530

    curosel

--- api/resources/app/tasks/getTasks.php
+++ api/resources/app/tasks/getTasks.php
@@ -25,7 +25,7 @@ function getTasks($appId){
         try {
             
             $data = httpGet($fetch_url);
-            var_dump($data['root']['srs'][0]); die();
+            var_dump($data); die();
             
         } catch(Exception $e) {
             echo '{"error":{"text":' . $e->getMessage() . '}}';
